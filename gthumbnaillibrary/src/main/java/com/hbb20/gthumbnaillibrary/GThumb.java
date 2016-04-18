c48e7eac8b8f76ec461afb0193edae6b0dedc10a
HBB20 <harshbbhakta@gmail.com>
2016-04-18 16:04:49 +0530

    - TextSize from xml

--- gthumbnaillibrary/src/main/java/com/hbb20/gthumbnaillibrary/GThumb.java
+++ gthumbnaillibrary/src/main/java/com/hbb20/gthumbnaillibrary/GThumb.java
@@ -1,11 +1,15 @@
 package com.hbb20.gthumbnaillibrary;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by hbb20 on 18/4/16.
@@ -14,6 +18,9 @@ public class GThumb extends RelativeLayout {
 
     Context context;
     View rootView;
+    RelativeLayout relativeForeground;
+    TextView textViewInitials;
+    ImageView imageViewRealImage,imageViewColorBg;
     private static String TAG="GThumb :";
     public GThumb(Context context) {
         super(context);
@@ -34,8 +41,49 @@ public class GThumb extends RelativeLayout {
     }
 
     private void init(AttributeSet attrs){
-        Log.d(TAG,"Initialization");
+        log("Initialization");
         rootView= LayoutInflater.from(context).inflate(R.layout.layout_user_thumb,this,true);
+        relativeForeground=(RelativeLayout) rootView.findViewById(R.id.relative_foreground);
+        textViewInitials=(TextView) rootView.findViewById(R.id.textView_initials);
+        imageViewRealImage=(ImageView) rootView.findViewById(R.id.imageView_real);
+        imageViewColorBg=(ImageView) rootView.findViewById(R.id.imageView_color_bg);
+        applyCustomAttributes(attrs);
+    }
+
+    private void applyCustomAttributes(AttributeSet attrs) {
+        log("Applying custom attribute");
+        TypedArray a= context.getTheme().obtainStyledAttributes(attrs,R.styleable.GThumb,0,0);
+        try{
+
+            //custom text size
+            int textSize=a.getDimensionPixelSize(R.styleable.GThumb_textSize,Math.round(12 * (context.getResources().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT)));
+            setTextSize(textSize);
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            log("Error in applying custom attributes");
+        }
+    }
+
+    private int getFitSize() {
+        int minDim=getWidth();
+        if(minDim>getHeight()){
+            minDim=getHeight();
+        }
+        return minDim==0?25:minDim;
+    }
+
+    /**
+     * This will update the size of text
+     * @param textSize size of text in pixel
+     */
+    public void setTextSize(int textSize) {
+        textViewInitials.setTextSize(textSize);
+    }
+
+    private void log(String s) {
+        Log.d(TAG,s);
     }
 
 }
